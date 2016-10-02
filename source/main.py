9d6329842be49dc8810914ed58238b46a83e9766
Rahul <Rahul@RahulChidurala>
2016-10-02 18:24:02 -0500

    Add file path/directory to GUI code

--- source/main.py
+++ source/main.py
@@ -3,6 +3,10 @@ Created on Sep 28, 2016
 
 @author: Rahul
 '''
+import os.path, sys
+# Add current dir to search path.
+sys.path.insert(0, "GUI")
+
 import GuiDisplay as gui
 
 if __name__ == '__main__':
